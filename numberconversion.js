5a09f4d62299f7c65c97f95026a33aeeb7e8b6a6
dynooooo1 <dylandominno7@gmail.com>
2024-09-15 22:31:30 +0800

    Create numberconversion.js

---
+++ numberconversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let v = parseInt(price);
+console.log("Using parseInt():", v);
+let A = Number(price);
+console.log("Using Number():", A);
+let 1 = +price;
+console.log("Using operator:", 1);
+
+
+let distance = 150;
+let o = distance.toString();
+console.log("Using toString():", 0);
+let r = String(distance);
+console.log("Using String():", r);
+
+
+let weight = "72.5";
+let i = parseFloat(weight);
+console.log("Using parseFloat():", i);
